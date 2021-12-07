49bbe1cb900f738e853c06e16c29d3d441497c52
Sunaga0709 <tsunaga0709@gmail.com>
2021-12-07 22:51:16 +0900

    add Pagy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+  Message.create(title:"title#{i+1}", context:"content#{i+1}")
+end
